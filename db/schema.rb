40afe96b6262c7286257d945f387caac2fa7b092
Jonas Capelier <capelier.jonas@gmail.com>
2025-05-28 00:42:43 +0200

    Added a Game model with title overview platform genre artwork_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_100357) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_27_224008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_100357) do
     t.datetime "updated_at", null: false
     t.index ["offer_id"], name: "index_bookings_on_offer_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.string "platform"
+    t.text "overview"
+    t.string "genre"
+    t.string "artwork_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "offers", force: :cascade do |t|
