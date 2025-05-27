55673b0e175a1173b0889c4aba13b0fcfe246053
Lorenz Houze <lorenz.houze@gmail.com>
2025-05-27 15:07:53 +0200

    add model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :offers, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone_number, presence: true, uniqueness: true
+  validates :address, presence: true
 end
