cb9f1bb14a192e96dba83d834b5454a8ea71a48a
Jonas Capelier <capelier.jonas@gmail.com>
2025-05-27 15:15:38 +0200

    Okay, I should have done way more commits, but here's the list: added gems Faraday and Faker, added API keys in .env, created seeds. In seeds comments, at the top, there is the way to get another access token when this one expires.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,96 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# response = Faraday.post("https://id.twitch.tv/oauth2/token?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&grant_type=client_credentials")
+# p response.status
+# p response.headers
+# p response.body
+
+response = Faraday.post("https://api.igdb.com/v4/games") do |req|
+  req.headers["Client-ID"] = ENV['CLIENT_ID']
+  req.headers["Authorization"] = ENV['ACCESS_TOKEN']
+
+  req.body = 'search "Tales of"; fields id, name; limit 50;'
+end
+
+results = JSON.parse(response.body)
+ids = results.map { |game| game["id"] }
+
+
+CONDITIONS = %w(Bad OK Good Mint)
+STATUS = %w(Pending Accepted Cancelled Past)
+
+puts "Cleaning DB"
+User.destroy_all
+Offer.destroy_all
+Booking.destroy_all
+puts "DB cleaned"
+
+puts "Creating the best users"
+User.create!({
+  first_name: "Lorenz",
+  last_name: "Houzé",
+  nickname: "The CEO",
+  address: "Here",
+  email: "ceo@oftheworld.com",
+  password: "password!!!"
+})
+
+
+User.create!({
+  first_name: "Mohamed",
+  last_name: "Hedi",
+  nickname: "The silent coder",
+  address: "There",
+  email: "sound@ofsilence.com",
+  password: "AZERTY!!!"
+})
+
+
+User.create!({
+  first_name: "Aude",
+  last_name: "Ahuat",
+  nickname: "The designer",
+  address: "Everywhere",
+  email: "colors@oftheweb.com",
+  password: "12345!!!"
+})
+
+User.create!({
+  first_name: "Chahnez",
+  last_name: "Zurawicz",
+  nickname: "The savior",
+  address: "Strawberry fields",
+  email: "save@everyone.com",
+  password: "6789!!!"
+})
+
+puts "Users created"
+
+puts "Creating offers"
+
+15.times do
+  Offer.create!({
+    description: Faker::Quotes::Shakespeare.romeo_and_juliet_quote,
+    rate_per_day: (1..10).to_a.sample,
+    condition: CONDITIONS.sample,
+    game_id: ids.sample,
+    user: User.all.sample
+  })
+end
+
+puts "Offers created"
+
+puts "Creating bookings"
+
+10.times do
+  start = Faker::Date.forward(days: rand(1..21))
+  finish = start + rand(1..7).days
+
+  Booking.create!({
+    user: User.all.sample,
+    offer: Offer.all.sample,
+    status: STATUS.sample,
+    start_date: start,
+    end_date: finish
+  })
+end
+
+puts "Bookings created"
