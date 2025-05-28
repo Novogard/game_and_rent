4800ea1e44f8c7eb81a514f724ae4bd33dbf83c6
Lorenz Houze <lorenz.houze@gmail.com>
2025-05-28 16:09:57 +0200

    Dahsboard view and offer delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, unless: :on_pages_dashboard
+
+  private
+
+  def on_pages_dashboard
+    controller_name == 'pages' && action_name == 'dashboard'
+  end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,7 @@
 class Offer < ApplicationRecord
   belongs_to :user
   belongs_to :game
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :description, presence: true, length: { minimum: 10 }
   validates :rate_per_day, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :condition, presence: true
