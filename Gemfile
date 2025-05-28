e4bb4513982bed087a7c46924e4cd7445315d395
Chahnez Zurawicz <cvkroyal@hotmail.com>
2025-05-28 16:17:50 +0200

    games/index + games/show

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ gem "devise"
 gem "faraday"
 gem "faker"
 gem "tailwindcss-rails"
+gem "geocoder"
 
 group :development, :test do
   gem "dotenv-rails"
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,18 @@
 class GamesController < ApplicationController
+
+  def index
+    @offers = Offer.all
+    @users = User.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :platform, :overview, :genre, :artwork_url)
+  end
+
 end
