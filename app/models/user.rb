69e5740824ba066aacad620650311853ab31143c
Jonas Capelier <capelier.jonas@gmail.com>
2025-05-28 08:52:04 +0200

    Updated the seeds. Delete the validation of the user's  phone_number, in User model, because it was inconsistent with the DB schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :phone_number, presence: true, uniqueness: true
   validates :address, presence: true
 end
