b61b9ec87868955b94a2cfd655c46ef38da4d240
Jonas Capelier <capelier.jonas@gmail.com>
2025-05-29 22:25:24 +0200

    WIP

--- app/javascript/controllers/filters_controller.js
+++ app/javascript/controllers/filters_controller.js
@@ -7,6 +7,42 @@ export default class extends Controller {
   }
 
   filter(event) {
-    this.eventTargen
+    const genreCheckBoxes = document.querySelectorAll('.genre-checkbox');
+    const selectedGenres = [];
+    const platformCheckBoxes = document.querySelectorAll('.platform-checkbox')
+    const selectedPlatforms = [];
+
+    genreCheckBoxes.forEach(checkbox => {
+      if(checkbox.checked) {
+        selectedGenres.push(checkbox.value)
+      }
+    })
+
+    platformCheckBoxes.forEach(checkbox => {
+      if(checkbox.checked) {
+        selectedPlatforms.push(checkbox.value)
+      }
+    })
+
+    const genreQuery = []
+    selectedGenres.forEach(genre => {
+      genreQuery.push('genre[]=' + `${genre}`)
+    })
+    const genreQueryString = genreQuery.join('&')
+
+    const platformQuery = []
+    selectedPlatforms.forEach(platform => {
+      platformQuery.push('platform[]=' + `${platform}`)
+    })
+    const platformQueryString = platformQuery.join('&')
+
+    const query = genreQueryString + '&' + platformQueryString
+
+    const url = `/games/filter?${query}`
+    fetch(url)
+      .then(response => response.text())
+      .then(html => {
+        document.querySelector("#results").innerHTML = html;
+    });
   }
 }
