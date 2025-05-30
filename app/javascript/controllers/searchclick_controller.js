6418c0c00f21657bbe10debe58a666db6e767c68
Mohamed hedi Gharbi <113791432+M-Hedi@users.noreply.github.com>
2025-05-30 14:54:17 +0200

    feature/offers-iamge-done

--- app/javascript/controllers/searchclick_controller.js
+++ app/javascript/controllers/searchclick_controller.js
@@ -5,7 +5,8 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static values = {
     title: String,
-    id: Number
+    id: Number,
+    artworkUrl: String
   }
 
   connect() {
@@ -17,6 +18,7 @@ export default class extends Controller {
     event.preventDefault()
     const selectedTitle = this.titleValue
     const gameId = this.idValue
+    const imageUrl = this.artworkUrlValue
     console.log (gameId)
 
     //  remplir un champ texte avec le titre du jeu
@@ -30,6 +32,15 @@ export default class extends Controller {
     gameInput.value = gameId
     }
 
+
+        // Afficher l'image dans la colonne droite
+    const preview = document.querySelector("#selected-game-preview")
+    console.log("artworkUrlValue:", this.artworkUrlValue)
+
+    if (preview && this.artworkUrlValue) {
+      preview.innerHTML = `
+        <img src="${this.artworkUrlValue}" alt="Image du jeu" class="w-auto h-80 object-contain rounded-lg shadow-md rounded-lg border border-gray-300 shadow-lg">`
+    }
 
 
   }
